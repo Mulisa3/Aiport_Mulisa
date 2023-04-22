1fe338cc1b9d5d616f2d137c80e226014890035f
Mulisa3 <115104071+Mulisa3@users.noreply.github.com>
2023-04-22 19:51:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,13 +45,13 @@ m = folium.Map(location=[airports.latitude.mean(), airports.longitude.mean()],
 #Loop through each row in the dataframe
 for i,row in airports.iterrows():
     #Setup the content of the popup
-    iframe = folium.IFrame('country:' + str(row["country"]))
+    iframe = folium.IFrame('altitude:' + str(row["altitude"]))
     
     #Initialise the popup using the iframe
     popup = folium.Popup(iframe, min_width=300, max_width=300)
     
     #Add each row to the map
     folium.Marker(location=[row['latitude'],row['longitude']],
-                  popup = popup, c=row['country']).add_to(m)
+                  popup = popup, c=row['altitude']).add_to(m)
 
 st_data = st_folium(m, width=700)
