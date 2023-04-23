2a48243ccd96f23b8aa63cc9d9e941b5f5f3c1cd
Mulisa3 <115104071+Mulisa3@users.noreply.github.com>
2023-04-23 23:24:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ import folium
 
 st.dataframe(airports)
 
-m = leafmap.Map(center=(-31.416668, -64.183334), zoom=5)
+m = leafmap.Map(center=(8.7832, 34.5085), zoom=5)
 for index, row in airports.iterrows():
     popup = folium.Popup(f"<strong>City:</strong> {row['city']}<br><strong>Airport:</strong> {row['airport']}<br><strong>Altitude:</strong> {row['altitude']} m")
     folium.Marker([row['latitude'], row['longitude']], popup=popup).add_to(m)
